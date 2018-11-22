7ce429a67bacadc6b85995eaaccaf8e0539f0028
Admin <Admin@PAC-SSO38AJFD99>
2018-11-22 13:26:17 +0700

    Add project files.

---
+++ DAL/getData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace DAL
+{
+    public class GetData : DBConnect
+    {
+        public int dangNhap(string id, string pass)
+        {
+            openC();
+            SqlCommand cmd = new SqlCommand("checkLogin", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@user", id));
+            cmd.Parameters.Add(new SqlParameter("@pass", id));
+            int kq = (int)cmd.ExecuteScalar();
+            closeC();
+            return kq;
+        }
+    }
+}

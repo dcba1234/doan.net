56825914d6835711c8b6be669ae48bea5406449c
Admin <Admin@PAC-SSO38AJFD99>
2018-11-29 16:30:26 +0700

    BUS & DAL

---
+++ DAL/DAL_loThuoc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Object;
+namespace DAL
+{
+    public class DAL_loThuoc : DBConnect
+    {
+
+        public DataTable select()
+        {
+            openC();
+            DataTable tb = new DataTable();
+            SqlCommand cmd = new SqlCommand("sp_select_loThuoc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(tb);
+            closeC();
+            return tb;
+
+        }
+        public void insert(loThuoc l)
+        {
+            openC();
+
+            SqlCommand cmd = new SqlCommand("sp_insert_loThuoc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@solothuoc", l.Solothuoc));
+            cmd.Parameters.Add(new SqlParameter("@ngaynhap", l.Ngaynhap));
+            cmd.Parameters.Add(new SqlParameter("@maNV", l.Ngaynhap));
+            cmd.Parameters.Add(new SqlParameter("@mancc", l.Mancc));
+
+            cmd.ExecuteNonQuery();
+
+            closeC();
+        }
+        public void update(loThuoc l)
+        {
+            openC();
+
+            SqlCommand cmd = new SqlCommand("sp_update_loThuoc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@solothuoc", l.Solothuoc));
+            cmd.Parameters.Add(new SqlParameter("@ngaynhap", l.Ngaynhap));
+            cmd.Parameters.Add(new SqlParameter("@maNV", l.Ngaynhap));
+            cmd.Parameters.Add(new SqlParameter("@mancc", l.Mancc));
+
+            cmd.ExecuteNonQuery();
+
+            closeC();
+        }
+        public void delete(loThuoc l)
+        {
+            openC();
+
+            SqlCommand cmd = new SqlCommand("sp_delete_loThuoc", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@solothuoc", l.Solothuoc));
+
+            cmd.ExecuteNonQuery();
+
+            closeC();
+        }
+    }
+
+}
---
+++ Object/loThuoc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+   public  class loThuoc
+    {
+        private string solothuoc;
+        private string ngaynhap;
+        private string maNV;
+        private string mancc;
+
+        public string Solothuoc
+        {
+            get
+            {
+                return solothuoc;
+            }
+
+            set
+            {
+                solothuoc = value;
+            }
+        }
+
+        public string Ngaynhap
+        {
+            get
+            {
+                return ngaynhap;
+            }
+
+            set
+            {
+                ngaynhap = value;
+            }
+        }
+
+        public string MaNV
+        {
+            get
+            {
+                return maNV;
+            }
+
+            set
+            {
+                maNV = value;
+            }
+        }
+
+        public string Mancc
+        {
+            get
+            {
+                return mancc;
+            }
+
+            set
+            {
+                mancc = value;
+            }
+        }
+    }
+}

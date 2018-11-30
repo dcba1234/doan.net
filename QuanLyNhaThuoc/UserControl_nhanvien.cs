34bd5ac028a8f1e9eb05411aba7e0cc30be36fff
Thành Tiến <tienthanh2171998@gmail.com>
2018-11-30 10:46:47 +0700

    s

---
+++ QuanLyNhaThuoc/UserControl_nhanvien.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BUS,
+using Object,
+namespace QuanLyNhaThuoc
+{
+    public partial class UserControl_nhanvien : UserControl
+    {
+        private BUS_nhanvien data_nv = new BUS_nhanvien();
+        private nhan
+        public UserControl_nhanvien()
+        {
+            InitializeComponent();
+        }
+    }
+}
